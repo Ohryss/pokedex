b9fe80cd1410f3f4979f88ee7b0253510ef143c7
Ohryss <trochain.thomas@outlook.com>
2024-12-20 09:19:30 +0100

    Update TypeInteraction.php

--- app/Models/TypeInteraction.php
+++ app/Models/TypeInteraction.php
@@ -16,14 +16,16 @@ class TypeInteraction extends Model
     {
         return $this->belongsTo(Type::class, 'from_type_id');
     }
+// app/Models/TypeInteraction.php
+public function toType()
+{
+    return $this->belongsTo(Type::class, 'to_type_id');
+}
 
-    public function to_type_id()
-    {
-        return $this->belongsTo(Type::class, 'to_type_id');
-    }
+public function state()
+{
+    return $this->belongsTo(TypeInteractionState::class, 'type_interaction_state_id');
+}
 
-    public function state()
-    {
-        return $this->belongsTo(TypeInteractionState::class);
-    }
+
 }
